a4b5004d6e8b037b1bca1fba5ed26aa4154b7937
Riley E. Chandler <riley@netandnix.net>
2024-02-20 01:10:25 -0500

    ESP info and working file

--- esp8266/Readme.md
+++ esp8266/Readme.md
@@ -1,1 +1,35 @@
  I can barely hack together the things I need, but it works...
+
+This display code is working now.
+The ESP connects to a Defined WiFi network, and connects to a local NTP server.
+It displays the local time, and UTC time with seconds.
+Then is subscribes to 6 MQTT topics and displays what is there on different parts of the display.
+The loader scripts will rename each station and assign it a Station number.
+That station number will stay in the same location.
+With a 20 character wide display, you're limited to what you can display.
+
+##Parts I used
+Depending how you make these, you can make them for about $10 per display.
+I'll try to add a case and pictures when I'm done
+###ESP8266
+HiLetgo 5pcs Mini ESP8266 ESP-12F Mini NodeMCU Lua 4M Bytes WiFi Module with Pin Headers
+https://www.amazon.com/gp/product/B073CQVFLK
+
+###Display
+I2C 2004 LCD Module | 20x04 LCD Screen Module 
+https://www.amazon.com/gp/product/B0C1G9GBRZ
+
+##Localization
+
+Chage the values for your local network.
+"<SSID>"  would be changed to "SuperAwesomeContestNet"
+ETC...
+
+const char* ssid = "<SSID>";
+const char* password = "<Network Key>";
+const char* mqtt_server = "<MQTT Server>";
+NTPClient timeClient(ntpUDP, "<NTP SERVER>", -5 * 3600, 60000); // UTC offset for Eastern Time is -5 hours (-5 * 3600 seconds)
+
+
+##Arduino IDE
+I've added so many libraries to my Arduino IDE, make sure you add everything in the include statements at the top of the file.
--- esp8266/n1mm_view_radio.ino
+++ esp8266/n1mm_view_radio.ino
@@ -1,141 +1,110 @@
-
-#ifdef ESP8266
-#include <ESP8266WiFi.h>
-#else
-#include <WiFi.h>
-#endif
-#include <time.h>
 #include <PubSubClient.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
 
+// Define LCD parameters
 int lcdColumns = 20;
 int lcdRows = 4;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+// Define Wi-Fi credentials
+const char* ssid = "<SSID>";
+const char* password = "<Network Key>";
 
-
-
-const char* ssid = "wless-guest";
-const char* password = "abc123def456!";
-
-const char* NTP_SERVER = "192.168.1.123";
-const char* TZ_INFO    = "EST5EDT";  // enter your time zone (https://remotemonitoringsystems.ca/time-zone-abbreviations.php)
-const char* TZ_INFO2    = "UTC"; 
-
-tm timeinfo;
-time_t now;
-long unsigned lastNTPtime;
-unsigned long lastEntryTime;
-
-// MQTT broker
-const char* mqtt_server = "192.168.1.202";
-const int mqtt_port = 1883; // Default MQTT port
-//const char* mqtt_username = "YourMQTTUsername";
-//const char* mqtt_password = "YourMQTTPassword";
-// MQTT topic to subscribe to
+// Define MQTT parameters
+const char* mqtt_server = "<MQTT Server>";
+const int mqtt_port = 1883;
 const char* mqtt_topic = "n1mm_radio/stations";
+const int num_topics = 6;
+const char* mqtt_topics[] = {
+  "n1mm_radio/stations/1",
+  "n1mm_radio/stations/2",
+  "n1mm_radio/stations/3",
+  "n1mm_radio/stations/4",
+  "n1mm_radio/stations/5",
+  "n1mm_radio/stations/6"
+};
+String receivedMessages[num_topics];
 
+// Define NTP parameters
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "<NTP SERVER>", -5 * 3600, 60000); // UTC offset for Eastern Time is -5 hours (-5 * 3600 seconds)
+
+// Wi-Fi client and MQTT client
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Variable to store the received message
-String receivedMessage = "";
-
-
+// Function prototypes
+void reconnect();
+void callback(char* topic, byte* payload, unsigned int length);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("\n\nNTP Time Test\n");
   WiFi.begin(ssid, password);
 
-
-
-
-  int counter = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(200);
-    if (++counter > 100) ESP.restart();
-    Serial.print ( "." );
-  // initialize LCD
+  // Initialize LCD
   lcd.init();
-  // turn on LCD backlight                      
   lcd.backlight();
   lcd.clear();
+
+  // Initialize NTP client
+  timeClient.begin();
+
+  // Set MQTT server and callback function
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
-  // Connect to MQTT broker
+
+  // Attempt to connect to Wi-Fi and MQTT
   reconnect();
-
-  }
-  Serial.println("\n\nWiFi connected\n\n");
-
-  configTime(0, 0, NTP_SERVER);
-  // See https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv for Timezone codes for your region
-  setenv("TZ", TZ_INFO, 1);
-
-  if (getNTPtime(10)) {  // wait up to 10sec to sync
-  } else {
-    Serial.println("Time not set");
-    ESP.restart();
-  }
-  showTime(timeinfo);
-  lastNTPtime = time(&now);
-  lastEntryTime = millis();
 }
 
-
 void loop() {
-  //MQTT Connect
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
-  // getTimeReducedTraffic(3600);
-  getNTPtime(10);
-  showTime(timeinfo);
-  delay(1000);
-  lcd.setCursor(0,1);
-  lcd.print(receivedMessage);
-}
-
-bool getNTPtime(int sec) {
-
-  {
-    uint32_t start = millis();
-    do {
-      time(&now);
-      localtime_r(&now, &timeinfo);
-      Serial.print(".");
-      delay(10);
-    } while (((millis() - start) <= (1000 * sec)) && (timeinfo.tm_year < (2016 - 1900)));
-    if (timeinfo.tm_year <= (2016 - 1900)) return false;  // the NTP call was not successful
-    Serial.print("now ");  Serial.println(now);
-    char time_output[30];
-    strftime(time_output, 30, "%a  %d-%m-%y %T", localtime(&now));
-    Serial.println(time_output);
-    Serial.println();
+  // Handle MQTT messages
+  for (int i = 0; i < num_topics; i++) {
+    lcd.setCursor(i % 2 * 11, i / 2 + 1);
+    lcd.print(receivedMessages[i]);
   }
-  return true;
 
+  // Update NTP time
+  timeClient.update();
+  
+  // Display Eastern time
+  lcd.setCursor(0, 0);
+  lcd.print("L-");
+  lcd.print(timeClient.getFormattedTime().substring(0, 5)); // Display time HH:MM
 
+  // Display UTC time
+  lcd.setCursor(10, 0);
+  lcd.print("U-");
+  lcd.print(timeClient.getFormattedTime());
+  
+  delay(1000); // Adjust delay as needed
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  // Callback function to handle received messages
   Serial.print("Message received on topic: ");
   Serial.println(topic);
-
-  Serial.print("Message: ");
-  receivedMessage = "";
-
+  String message = "";
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-    receivedMessage += (char)payload[i];
-    lcd.clear();
-
+    message += (char)payload[i];
   }
-  Serial.println();
-
+  Serial.print("Message: ");
+  Serial.println(message);
+  // Store the received message in the receivedMessages array
+  for (int i = 0; i < num_topics; i++) {
+    if (strcmp(topic, mqtt_topics[i]) == 0) {
+      receivedMessages[i] = message;
+      break;
+    }
+  }
 }
 
 void reconnect() {
@@ -143,7 +112,9 @@ void reconnect() {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
-      client.subscribe(mqtt_topic);
+      for (int i = 0; i < num_topics; i++) {
+        client.subscribe(mqtt_topics[i]);
+      }
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -152,49 +123,3 @@ void reconnect() {
     }
   }
 }
-
-
-void showTime(tm localTime) {
-  Serial.print(localTime.tm_mon + 1);
-  Serial.print('/');
-  Serial.print(localTime.tm_mday);
-  Serial.print('/');
-  Serial.print(localTime.tm_year - 100);
-  Serial.print('-');
-  Serial.print(localTime.tm_hour);
-  Serial.print(':');
-  Serial.print(localTime.tm_min);
-  Serial.print(':');
-  Serial.print(localTime.tm_sec);
-  //Printing Date and Time to LCD
-  lcd.setCursor(0,0);
-  lcd.print(localTime.tm_mon + 1);
-  lcd.setCursor(2,0);
-  lcd.print('/');
-  lcd.setCursor(3,0);
-  lcd.print(localTime.tm_mday);
-  lcd.setCursor(5,0);
-  lcd.print('/');
-  lcd.setCursor(6,0);
-  lcd.print(localTime.tm_year - 100);
-  lcd.setCursor(8,0);
-  lcd.print(' ');
-  lcd.setCursor(9,0);
-  lcd.print(localTime.tm_hour);
-  lcd.setCursor(11,0);
-  lcd.print(':');
-  lcd.setCursor(12,0);
-  lcd.print(localTime.tm_min);
-  lcd.setCursor(14,0);
-  lcd.print('U');
-  lcd.setCursor(15,0);
-  lcd.print(localTime.tm_hour + 5);
-  lcd.setCursor(17,0);
-  lcd.print(':');
-  lcd.setCursor(18,0);
-  lcd.print(localTime.tm_min);
-
-
-}
-
-
